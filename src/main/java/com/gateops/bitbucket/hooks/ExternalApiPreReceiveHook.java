dd212e2466d0ac135ec990d989db519800a04c1c
Emrullah YILDIRIM <emrullah.yildirim@windowslive.com>
2025-05-15 02:18:15 +0300

    MILLSTONE v1 commit -head

--- src/main/java/com/gateops/bitbucket/hooks/ExternalApiPreReceiveHook.java
+++ src/main/java/com/gateops/bitbucket/hooks/ExternalApiPreReceiveHook.java
@@ -4,7 +4,6 @@ import com.atlassian.bitbucket.hook.repository.PreRepositoryHook;
 import com.atlassian.bitbucket.hook.repository.PreRepositoryHookContext;
 import com.atlassian.bitbucket.hook.repository.RepositoryHookResult;
 import com.atlassian.bitbucket.hook.repository.RepositoryPushHookRequest;
-import com.atlassian.bitbucket.setting.Settings;
 
 import javax.annotation.Nonnull;
 
@@ -14,16 +13,8 @@ public class ExternalApiPreReceiveHook implements PreRepositoryHook<RepositoryPu
     @Override
     public RepositoryHookResult preUpdate(@Nonnull PreRepositoryHookContext context,
                                           @Nonnull RepositoryPushHookRequest request) {
-        Settings settings = context.getSettings();
-        String apiUrl = settings.getString("apiUrl", "");
-        String branchesConfig = settings.getString("branches", "");
-        if (apiUrl.isEmpty() || branchesConfig.isEmpty()) {
-            return RepositoryHookResult.accepted();
-        }
 
-        return RepositoryHookResult.rejected(
-                "External API call failed",
-                "Push rejected: ");
+        return RepositoryHookResult.rejected("External API call failed", "Push rejected: ");
 
     }
 }
